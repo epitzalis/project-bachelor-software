f0008e400d64732613c33b9188ccfb5c4615a1be
epitzalis <epitzalism@gmail.com>
2021-09-18 17:22:33 +0200

    Created calculator dto

---
+++ src/app/components/calculator/models/calculator.dto.ts
@@ -0,0 +1,13 @@
+export type Calculator = 'boole' | 'proposition'
+
+export enum ConversionTypeCalculator {
+    'p' = 'a',
+    'q' = 'b',
+    'r' = 'c',
+    's' = 'd',
+    '~' = '¬',
+    'v' = '+',
+    '^' = 'x',
+}
+
+export const universalVariables = ['a', 'b', 'c', 'd']
