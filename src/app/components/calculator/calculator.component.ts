854ba02d50331bc134afc6e37906b28a5eee1584
epitzalis <epitzalism@gmail.com>
2021-09-19 16:51:32 +0200

    The buttons set a value in the input

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,5 +1,5 @@
 import {
-  Component, ElementRef, Input, ViewChild,
+  Component, ElementRef, Input, Renderer2, ViewChild,
 } from '@angular/core'
 import { Calculator } from './models/calculator.dto'
 
@@ -12,4 +12,35 @@ export class CalculatorComponent {
   @Input() public typeCalculator: Calculator = 'boole'
 
   @ViewChild('inputCalculator', { static: false }) inputCalculator: ElementRef<HTMLInputElement>
+
+  constructor(
+    private readonly renderer: Renderer2,
+  ) {}
+
+  get valueCalculator(): string {
+    return this.inputCalculator.nativeElement.value
+  }
+
+  set valueCalculator(newValue: string) {
+    this.renderer.setProperty(this.inputCalculator.nativeElement, 'value', newValue)
+  }
+
+  public onAddCharacter(character: string): void {
+    const oldValue = this.valueCalculator
+    this.valueCalculator = oldValue + character
+  }
+
+  public onBack(): void {
+    this.valueCalculator = this.valueCalculator.slice(0, -1)
+  }
+
+  public onClear(): void {
+    this.valueCalculator = ''
+  }
+
+  public onCalculate(): void {
+    if (this.valueCalculator) {
+      // TODO
+    }
+  }
 }
