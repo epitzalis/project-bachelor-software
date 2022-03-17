77c9d31a4ad3834843af76ef6d473211b9c7cc14
epitzalis <epitzalism@gmail.com>
2022-03-17 23:36:57 +0100

    Added tests to proposition-info

--- src/app/components/proposition-info/proposition-info.component.spec.ts
+++ src/app/components/proposition-info/proposition-info.component.spec.ts
@@ -1,14 +1,7 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { TranslateModule } from '@ngx-translate/core'
+import { TranslateModule, TranslateService } from '@ngx-translate/core'
 import { PropositionInfoComponent } from './proposition-info.component'
-
-const rowsMock = [
-  ['F', 'F', 'F'],
-  ['F', 'V', 'F'],
-  ['V', 'F', 'F'],
-  ['V', 'V', 'V'],
-]
 
 describe('PropositionInfoComponent', () => {
   let component: PropositionInfoComponent
@@ -22,6 +15,9 @@ describe('PropositionInfoComponent', () => {
       declarations: [
         PropositionInfoComponent,
       ],
+      providers: [
+        TranslateService,
+      ],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents()
   })
@@ -29,11 +25,52 @@ describe('PropositionInfoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PropositionInfoComponent)
     component = fixture.componentInstance
-    component['rows'] = rowsMock
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('loadInfo is and returns text of contradiction', () => {
+    const rowsMock = [
+      ['F', 'F', 'F'],
+      ['F', 'V', 'F'],
+      ['V', 'F', 'F'],
+      ['V', 'V', 'F'],
+    ]
+    component['rows'] = rowsMock
+    const service = TestBed.inject(TranslateService)
+    const spy = spyOn(service, 'instant').and.callFake(() => null)
+    component['loadInfo']()
+    expect(spy).toHaveBeenCalledWith('RESULT.CONTRADICTION')
+  })
+
+  it('loadInfo is and returns text of tautology', () => {
+    const rowsMock = [
+      ['F', 'F', 'V'],
+      ['F', 'V', 'V'],
+      ['V', 'F', 'V'],
+      ['V', 'V', 'V'],
+    ]
+    component['rows'] = rowsMock
+    const service = TestBed.inject(TranslateService)
+    const spy = spyOn(service, 'instant').and.callFake(() => null)
+    component['loadInfo']()
+    expect(spy).toHaveBeenCalledWith('RESULT.TAUTOLOGY')
+  })
+
+  it('loadInfo is and returns text of contingency', () => {
+    const rowsMock = [
+      ['F', 'F', 'F'],
+      ['F', 'V', 'F'],
+      ['V', 'F', 'F'],
+      ['V', 'V', 'V'],
+    ]
+    component['rows'] = rowsMock
+    const service = TestBed.inject(TranslateService)
+    const spy = spyOn(service, 'instant').and.callFake(() => null)
+    component['loadInfo']()
+    expect(spy).toHaveBeenCalledWith('RESULT.CONTINGENCY')
   })
 })
